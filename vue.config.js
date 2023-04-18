14a07cfde601ef3b3157673780545ba6d48fb205
cebridani <123812489+cebridani@users.noreply.github.com>
2023-04-18 19:12:13 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,17 @@
 module.exports = {
-  devServer: {
-    host: '0.0.0.0',
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:5000',
-        changeOrigin: true,
-        pathRewrite: {
-          '^/api': ''
-        }
-      }
-    }
-  },
-  transpileDependencies: true,
-  lintOnSave: false
-}
+	  devServer: {
+		host: '0.0.0.0',
+		port: 3000,
+		proxy: {
+		  '/api': {
+			target: 'http://backend-chess:80',
+			changeOrigin: true,
+			pathRewrite: {
+			  '^/api': ''
+			}
+		  }
+		}
+	  },
+	  transpileDependencies: true,
+	  lintOnSave: false
+	}
