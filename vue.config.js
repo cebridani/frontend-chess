5192cd12cf59dcee54caf41209d1f5c66bac346e
cebridani <123812489+cebridani@users.noreply.github.com>
2023-05-15 19:10:26 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   devServer: {
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:5000",
+        target: "http://192.168.49.2:30353",
         changeOrigin: true,
       },
     },
